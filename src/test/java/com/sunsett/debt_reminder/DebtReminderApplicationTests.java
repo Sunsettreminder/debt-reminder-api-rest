9ec71611cb7dfed3b0c10b88140b6ebccf165f9e
Jair Araujo <jaraujod1@upao.edu.pe>
2024-07-06 13:33:15 -0500

    Estructuración y documentación del proyecto

---
+++ src/test/java/com/sunsett/debt_reminder/DebtReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunsett.debt_reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
